a0b1a90f277fd369e35d942448b0e086ee1657ee
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-08-28 21:50:38 +0300

    plantsy code challenge

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({onAddPlant}) {
+  const[name,setName] =useState("");
+  const[image,setImage] =useState("");
+  const[price,setPrice] =useState("");
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const newPlant = { name, image, price };
+    fetch("http://localhost:6001/plants",{
+      method:'POST',
+      headers:{
+        'Content-Type':'Application/JSON'
+      },
+      body: JSON.stringify(newPlant)
+    })
+    .then((res)=>res.json())
+    .then((plant)=>{
+      onAddPlant(plant) 
+      setName('')
+       setImage('')
+        setPrice('')
+    })
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={name} onChange={(event) => setName(event.target.value)} />
+        <input type="text" name="image" placeholder="Image URL" value={image} onChange={(event) => setImage(event.target.value)} />
+        <input type="number" name="price" step="0.01" placeholder="Price" value={price} onChange={(event) => setPrice(event.target.value)} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
